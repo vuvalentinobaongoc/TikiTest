e8c15d235449dddee2c93b272323c2b4cf483d63
Nicolas <valentinobaongoc@gmail.com>
2019-05-22 12:46:28 +0700

    Initial Commit

---
+++ TikiTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TikiTest
+//
+//  Created by Ngoc Vu on 5/22/19.
+//  Copyright © 2019 Ngoc Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
